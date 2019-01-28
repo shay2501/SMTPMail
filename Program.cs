8e4f3294f576fd151f7fcfe0ec0c6488ec3254c8
Shannon Whalen <swhalen@jsheld.com>
2019-01-28 14:04:05 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Utility;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var filepath = Directory.GetCurrentDirectory();
+            var file = Path.Combine(filepath, "oauthconfig.json");
+            AppSettings.ReadConfigurationSettings(file);
+
+            var name = "Shannon Whalen";
+            var email = "swhalen@jsheld.com";
+            var body = "Email Body";
+            var subject = "Email Subject";
+
+            SMTPMail.SendMail(body, subject, name, email, name, email);
+
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("Please type any key:");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ SMTPMail.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+/*
+ * Requires nuget packages:
+ * Mailkit
+ */
+
+namespace Utility
+{
+    public class SMTPMail
+    {
+        public static void SendMail(string body, string subject, string toname, string toaddress, string fromname, string fromaddress)
+        {
+            var message = new MimeMessage();
+
+            message.To.Add(new MailboxAddress(toname, toaddress));
+            message.From.Add(new MailboxAddress(fromname, fromaddress));
+            message.Subject = subject;
+
+            message.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using (var client = new SmtpClient())
+            {
+                //using the office 365 smtp smart relay server
+                try
+                {
+                    // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                    client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                    //office 365 smart mail relay
+                    client.Connect(AppSettings.serverName, AppSettings.serverPort, false);
+
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
